ef4d43b0f1c7adde81be03740eafdd98e524e817
DavoglioV <vanessadavoglio@gmail.com>
2018-04-01 01:26:20 -0300

    .

--- src/main/java/br/com/carrinhodecompra/CarrinhoDeCompraApplication.java
+++ src/main/java/br/com/carrinhodecompra/CarrinhoDeCompraApplication.java
@@ -2,10 +2,16 @@ package br.com.carrinhodecompra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 //fiz uma alteração
 @SpringBootApplication
 public class CarrinhoDeCompraApplication {
-
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(CarrinhoDeCompraApplication.class, args);
 	}
