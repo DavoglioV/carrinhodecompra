b6b9809c7b73047828a2f560ca2e333a61d0ca6f
DavoglioV <vanessadavoglio@gmail.com>
2018-04-01 22:04:53 -0300

    Alteração para permitir que outros sistemas usem a API

--- src/main/java/br/com/carrinhodecompra/controller/CarrinhoController.java
+++ src/main/java/br/com/carrinhodecompra/controller/CarrinhoController.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import br.com.carrinhodecompra.web.response.GenericResponse;
 
 @RestController
 @RequestMapping("/carrinho")
+@CrossOrigin(origins="*")
 //@Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class CarrinhoController {
 
--- src/main/java/br/com/carrinhodecompra/controller/ProdutoController.java
+++ src/main/java/br/com/carrinhodecompra/controller/ProdutoController.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import br.com.carrinhodecompra.web.response.ProdutoResponse;
 
 @RestController
 @RequestMapping("/produto")
+@CrossOrigin(origins="*")
 public class ProdutoController {
 
 	@Autowired
